f573c3433cf8478ca4f0ec51532ed91f5e613f0e
TheRoot89 <toni.babik@gmx.de>
2017-10-11 07:49:23 +0200

    Included Server files in repository, see Wiki workspace entry

---
+++ Server_files/start.bat
@@ -0,0 +1,2 @@
+java -Xmx1024M -jar spigot-1.12.2.jar
+PAUSE
---
+++ Server_files/start_debug.bat
@@ -0,0 +1,2 @@
+java -Xmx1024M -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 -jar spigot-1.12.2.jar -o true 
+PAUSE
